7be1e30ed5946d43f406d09ecef568f622dc9d9c
Ethan <eldiep@ualberta.ca>
2025-10-30 11:44:15 -0600

    DeleteCityFail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,8 +8,20 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-        // This will fail initially because hasCity() doesn't exist
+
         assertTrue(list.hasCity(calgary));
+    }
+
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        list.deleteCity(calgary);
+        // This will fail initially because deleteCity() doesn't exist
+        assertFalse(list.hasCity(calgary));
     }
 
 }
